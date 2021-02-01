c339607164fd281cb2a5ca542739814af5ee06b8
Muskan Punyani <57041968+muskanpunyani@users.noreply.github.com>
2021-02-01 23:18:57 +0530

    Update Online Stock Span.cpp

--- STACKS/Online Stock Span.cpp
+++ STACKS/Online Stock Span.cpp
@@ -1,6 +1,7 @@
 d returns the span of that stock's price for the current day.
 
-The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which
+the price of the stock was less than or equal to today's price.
 
 For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
 
@@ -37,7 +38,7 @@ Note:
 
 class StockSpanner {
 public:
-    stack<pair<int,int>> s;
+    stack<pair<int,int>> s;   // price and val
     StockSpanner() {
         
     }
