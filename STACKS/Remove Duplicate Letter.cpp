3ad1f195043adf85d5a2ac7cd76d4aa2b64e73fc
Muskan Punyani <57041968+muskanpunyani@users.noreply.github.com>
2021-02-01 23:26:27 +0530

    Update Remove Duplicate Letter.cpp

--- STACKS/Remove Duplicate Letter.cpp
+++ STACKS/Remove Duplicate Letter.cpp
@@ -1,4 +1,5 @@
-Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+Given a string s, remove duplicate letters so that every letter appears once and only once.
+You must make sure your result is the smallest in lexicographical order among all possible results.
 
 Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
 
@@ -55,7 +56,7 @@ public:
             }
            st.push(s[i]);
            m[s[i]]--;
-          visited[s[i]-'a']=true;
+           visited[s[i]-'a']=true;
          }
         string ans;
         while(st.empty()==false)
