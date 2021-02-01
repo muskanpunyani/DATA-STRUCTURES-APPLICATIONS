8793b6697816107157d695d1828b66881f53fea6
Muskan Punyani <57041968+muskanpunyani@users.noreply.github.com>
2021-02-01 19:10:57 +0530

    Update Find Eventual Safe States.cpp

--- GRAPH/Find Eventual Safe States.cpp
+++ GRAPH/Find Eventual Safe States.cpp
@@ -1,10 +1,13 @@
-In a directed graph, we start at some node and every turn, walk along a directed edge of the graph.  If we reach a node that is terminal (that is, it has no outgoing directed edges), we stop.
+In a directed graph, we start at some node and every turn, walk along a directed edge of the graph.  
+If we reach a node that is terminal (that is, it has no outgoing directed edges), we stop.
 
-Now, say our starting node is eventually safe if and only if we must eventually walk to a terminal node.  More specifically, there exists a natural number K so that for any choice of where to walk, we must have stopped at a terminal node in less than K steps.
+Now, say our starting node is eventually safe if and only if we must eventually walk to a terminal node.
+More specifically, there exists a natural number K so that for any choice of where to walk, we must have stopped at a terminal node in less than K steps.
 
 Which nodes are eventually safe?  Return them as an array in sorted order.
 
-The directed graph has N nodes with labels 0, 1, ..., N-1, where N is the length of graph.  The graph is given in the following form: graph[i] is a list of labels j such that (i, j) is a directed edge of the graph.
+The directed graph has N nodes with labels 0, 1, ..., N-1, where N is the length of graph.  
+The graph is given in the following form: graph[i] is a list of labels j such that (i, j) is a directed edge of the graph.
 
 Example:
 Input: graph = [[1,2],[2,3],[5],[0],[5],[],[]]
@@ -21,7 +24,7 @@ Note:
 
 *************************************************************************************************************************************
 
-
+// if we reach terminal node from any node,i.e not end up in cycle then that is safe
 class Solution {
 public:
     bool dfs(vector<int> &color,vector<vector<int>>& graph,int start)
@@ -46,6 +49,7 @@ public:
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
+        // initially color=0, 1 means=cycle,2 means terminal
         vector<int> color(n+1,0);
         vector<int> ans;
         for(int i=0;i<n;i++)    // check for every node,cycle or not
