ab3208595e6c8a21ced1bf9287a3793d554082ae
Muskan Punyani <57041968+muskanpunyani@users.noreply.github.com>
2021-12-11 21:01:59 +0530

    Create 1] LONGEST COMMON SUBSTRING.cpp

---
+++ DP/ADITYA_VERMA_DP/LCS/1] LONGEST COMMON SUBSTRING.cpp
@@ -0,0 +1,66 @@
+Given two strings. The task is to find the length of the longest common substring.
+
+
+Example 1:
+
+Input: S1 = "ABCDGH", S2 = "ACDGHR"
+Output: 4
+Explanation: The longest common substring
+is "CDGH" which has length 4.
+
+Example 2:
+
+Input: S1 = "ABC", S2 "ACB"
+Output: 1
+Explanation: The longest common substrings
+are "A", "B", "C" all having length 1.
+
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function longestCommonSubstr() which takes the string S1, string S2 and their length n and m as inputs and returns the length of the longest common substring in S1 and S2.
+
+
+Expected Time Complexity: O(n*m).
+Expected Auxiliary Space: O(n*m).
+
+
+Constraints:
+1<=n, m<=1000
+******************
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string s1, string s2)
+    {
+        // dp
+        // initialization
+        
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        // code
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
